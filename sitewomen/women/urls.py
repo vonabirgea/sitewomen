c2bd81efe2d1e2cd3afae4668bf2406ee34688d4
Greight <vonabirg.e.a@gmail.com>
2024-07-21 23:53:29 +0300

    Проект после 14 видео

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -8,7 +8,8 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path('', views.index, name="home"),    # http://127.0.0.1:8000/
     path('about/', views.about, name="about"),
-    path('cats/<int:cat_id>/', views.categories, name="cats_id"),  # http://127.0.0.1:8000/cats/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name="cats"),  # http://127.0.0.1:8000/cats/
-    path("archive/<year4:year>/", views.archive, name="archive")
+    path('addpage/', views.add_page, name="add_page"),
+    path('contact/', views.contact, name="contact"),
+    path('login/', views.login, name="login"),
+    path('post/<int:post_id>/', views.show_post, name="post"),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,55 +5,50 @@ from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
 
-class MyClass:
-    def __init__(self, a, b) -> None:
-        self.a = a
-        self.b = b
-
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+    
+]
 
 def index(request):  # HttpRequest
-    # t = render_to_string('women/index.html')  # две эти строчки делают то же самое, что и одна строчка сразу под ними
-    # return HttpResponse(t)
     data = {
-        'titile': 'Главная страница??',
-        'main_titile': "Заголовок",
+        'title': 'Главная страница',
+        'main_title': 'Главная страница',
         'menu': menu,
-        'float': 28.56,
-        'integer': 6,
-        'lst': [1, 2, 'abc', True],
-        'set': {1, 2, 3, 2, 5},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20),
-        'url': slugify("hey hou Lets GOU")
+        'posts': data_db,
         }
     return render(request, "women/index.html", context=data)
 
 
 def about(request):
-    data = {'titile': 'О сайте'}
-    return render(request, "women/about.html", data)
+    data = {'title': 'О сайте'}
+    return render(request, "women/about.html", {'title': 'О сайте', 'menu': menu})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def add_page(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(request, year):
-    if year > 2024:
-        uri = reverse("cats", args=("sport",))
-        return HttpResponseRedirect(uri)
+def contact(request):
+    return HttpResponse("Обратная связь")
 
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def page_not_found(request, exception):
-
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
