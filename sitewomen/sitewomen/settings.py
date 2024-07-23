a5b93d3b7c80c5e5a46e323b97f8d8152643e840
Greight <vonabirg.e.a@gmail.com>
2024-07-23 23:09:10 +0300

    ORM, миграции. После 19 видео

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'women.apps.WomenConfig'
+    'django_extensions',
+    'women.apps.WomenConfig',
 ]
 
 MIDDLEWARE = [
